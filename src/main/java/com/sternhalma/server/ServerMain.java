4065309ed9afc7dcb0083a5a927e38022b585fd9
artniewski <artniewski@gmail.com>
2021-12-20 11:12:54 +0100

    Add ServerMain command line options parsing
    Add required options: port and address
    using apache CLI (add to pom.xml)

--- src/main/java/com/sternhalma/server/ServerMain.java
+++ src/main/java/com/sternhalma/server/ServerMain.java
@@ -1,13 +1,43 @@
 package com.sternhalma.server;
 
+import org.apache.commons.cli.*;
+
 public class ServerMain {
 
-    public static final int DEFAULT_PORT = 25000;
-    public static final String DEFAULT_ADDRESS = "localhost";
-
     public static void main(String[] args) {
-        //TODO: Dodać parsowanie argumentów konsoli i odpowiednio zastąpić wartości domyślne.
-        Server server = new Server(DEFAULT_ADDRESS, DEFAULT_PORT);
-        server.start();
+        final int PORT;
+        final String ADDRESS;
+
+        Options options = new Options();
+        options.addOption(Option.builder("port")
+                .hasArg(true)
+                .desc("server PORT    [REQUIRED]")
+                .type(Number.class)
+                .required(true)
+                .build());
+        options.addOption(Option.builder("a")
+                .longOpt("address")
+                .hasArg(true)
+                .desc("server address [REQUIRED]")
+                .required(true)
+                .build());
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd;
+        try {
+            cmd = parser.parse(options, args);
+
+            PORT = ((Number) cmd.getParsedOptionValue("port")).intValue();
+            ADDRESS = cmd.getOptionValue("address");
+
+            Server server = new Server(ADDRESS, PORT);
+            System.out.println("Server running on PORT: " + PORT + " and address: " + ADDRESS);
+            server.start();
+
+        } catch (ParseException pe) {
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("ServerMain -port <arg> -a <arg>", options);
+            System.exit(1);
+        }
     }
 }
