a8713af9f997b398e747439aec24c2ea6d64b3cb
MizozPl <dox44@wp.pl>
2022-01-11 02:05:09 +0100

    Game.java javadoc

--- src/main/java/com/sternhalma/client/games/Game.java
+++ src/main/java/com/sternhalma/client/games/Game.java
@@ -2,7 +2,14 @@ package com.sternhalma.client.games;
 
 import com.sternhalma.client.connection.Client;
 
+/**
+ * Interfejs reprezentujący grę (po stronie klienta).
+ */
 public interface Game {
 
+    /**
+     * Inicjalizuje grę i przekazuje sterowanie.
+     * @param client handler do komunikacji z serwerem.
+     */
     void init(Client client);
 }
