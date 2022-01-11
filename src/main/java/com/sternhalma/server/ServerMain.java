aaeec5b43b427826f2d04e4238a25588f88cfa75
MizozPl <dox44@wp.pl>
2022-01-11 01:13:31 +0100

    ServerMain.java javadoc

--- src/main/java/com/sternhalma/server/ServerMain.java
+++ src/main/java/com/sternhalma/server/ServerMain.java
@@ -2,11 +2,24 @@ package com.sternhalma.server;
 
 import com.sternhalma.server.connection.Server;
 
+/**
+ * Klasa parsująca adres i port dla serwera oraz go uruchamiająca.
+ */
 public class ServerMain {
 
+    /**
+     * Domyślny port serwera.
+     */
     public static int PORT = 25000;
+    /**
+     * Domyślny adres serwera.
+     */
     public static String ADDRESS = "localhost";
 
+    /**
+     * Punkt wejściowy serwera.
+     * @param args - opcjonalne port(args[0]) i adres(args[1])
+     */
     public static void main(String[] args) {
         if (args.length == 2) {
             try {
