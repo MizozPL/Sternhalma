9c9b2e8e6000edc5d9b57c1a4a337e36dc2d32b4
MizozPl <dox44@wp.pl>
2022-01-11 02:10:17 +0100

    InfoPanel.java javadoc

--- src/main/java/com/sternhalma/client/games/basicsternhalma/gui/InfoPanel.java
+++ src/main/java/com/sternhalma/client/games/basicsternhalma/gui/InfoPanel.java
@@ -7,11 +7,24 @@ import javax.swing.*;
 import javax.swing.border.BevelBorder;
 import java.awt.*;
 
+/**
+ * Reprezentuje panel zawierający informację o kolorze gracza, turze i posiadający przycisk końca tury.
+ */
 public class InfoPanel extends JPanel {
 
+    /**
+     * Panel wyświetlający kolor gracza, którego jest tura.
+     */
     private final JPanel turnPanel;
+    /**
+     * Panel wyświetlający odpowiedni kolor dla danego gracza.
+     */
     private final JPanel playerTag;
 
+    /**
+     * Domyślny konstruktor inicjalizujący zawartość panelu.
+     * @param basicSternhalma referencja do obiektu obsługującego grę
+     */
     public InfoPanel(BasicSternhalma basicSternhalma) {
         setPreferredSize(new Dimension(0, 100));
         setLayout(new BorderLayout());
@@ -37,10 +50,18 @@ public class InfoPanel extends JPanel {
         add(spacing, BorderLayout.SOUTH);
     }
 
+    /**
+     * Aktualizuje panel z id gracza.
+     * @param playerID id gracza, którego kolor ma być wyświetlony.
+     */
     public void setPlayerID(int playerID) {
         playerTag.setBackground(Piece.getColorForPlayerWithID(playerID));
     }
 
+    /**
+     * Aktualizuje panel z kolorem tury
+     * @param playerID id gracza, którego jest tura
+     */
     public void setTurn(int playerID) {
         turnPanel.setBackground(Piece.getColorForPlayerWithID(playerID));
     }
