37379df1ea1181cdb7a6ecedc16617eeeb4e95a6
MizozPl <dox44@wp.pl>
2022-01-11 01:22:16 +0100

    Game.java javadoc

--- src/main/java/com/sternhalma/server/games/Game.java
+++ src/main/java/com/sternhalma/server/games/Game.java
@@ -2,10 +2,26 @@ package com.sternhalma.server.games;
 
 import com.sternhalma.server.connection.Player;
 
+/**
+ * Interfejs reprezentujący grę (po stronie serwera) przechowywaną w GameManager
+ */
 public interface Game {
+    /**
+     * Dołącza gracza o handlerze player do gry.
+     * @param player handler gracza
+     */
     void joinPlayer(Player player);
 
+    /**
+     * Wykonuje akcję określoną odpowiednio spreparowanym Stringiem action przez gracza o handlerze player.
+     * @param player handler gracza
+     * @param action String określający akcję
+     */
     void performAction(Player player, String action);
 
+    /**
+     * Zwraca unikalną nazwę gry - powinna być zapisana w pliku Games.java.
+     * @return unikalna nazwa gry
+     */
     String getGameName();
 }
