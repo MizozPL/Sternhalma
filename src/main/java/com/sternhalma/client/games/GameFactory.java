1b367f33356c38f6affdc58da89beca5b8cccbf6
MizozPl <dox44@wp.pl>
2022-01-11 02:03:52 +0100

    GameFactory.java javadoc

--- src/main/java/com/sternhalma/client/games/GameFactory.java
+++ src/main/java/com/sternhalma/client/games/GameFactory.java
@@ -3,12 +3,23 @@ package com.sternhalma.client.games;
 import com.sternhalma.client.games.basicsternhalma.BasicSternhalma;
 import com.sternhalma.common.games.Games;
 
+/**
+ * Klasa statyczna tworząca odpowiednie instancje gier.
+ */
 public class GameFactory {
 
+    /**
+     * Prywatny konstruktor.
+     */
     private GameFactory() {
 
     }
 
+    /**
+     * Tworzy instancję gry o danej nazwie (z Games.java)
+     * @param gameName nazwa gry
+     * @return instancję gry lub null, jeśli taka nie istnieje
+     */
     public static Game createGameInstance(String gameName) {
         switch (gameName) {
             case Games.BASIC_STERNHALMA -> {
