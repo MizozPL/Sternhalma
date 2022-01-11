99c134c3196a7783e7b47c4dea5cf7997c8e16d3
artniewski <artniewski@gmail.com>
2022-01-11 02:53:57 +0100

    Board javadoc

--- src/main/java/com/sternhalma/common/games/basicsternhalma/Board.java
+++ src/main/java/com/sternhalma/common/games/basicsternhalma/Board.java
@@ -6,17 +6,33 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
-import static java.lang.Math.abs;
-
+/**
+ * Klasa reprezentująca planszę gry, służąca do przechowywania i wymiany danych.
+ */
 public class Board implements Serializable {
 
+    /**
+     * Wymiary planszy do działań matematycznych. (magic numbers)
+     */
     public static final int BOARD_X = 25;
     public static final int BOARD_Y = 17;
 
+    /**
+     * Liczba graczy w obecnej grze.
+     */
     private int numberOfPlayers;
+    /**
+     * Mapa pionków wszystkich graczy wraz z pozycjami na planszy.
+     */
     private final HashMap<Point, Piece> piecesWithPosition;
+    /**
+     * Set wszystkich pozycji na planszy.
+     */
     private static final HashSet<Point> validPoints;
 
+    /**
+     * Sety pozycji startowych zawierających się w poszczególnych rogach mapy.
+     */
     private static final HashSet<Point> topBasePoints;
     private static final HashSet<Point> topRightBasePoints;
     private static final HashSet<Point> bottomRightBasePoints;
@@ -24,18 +40,24 @@ public class Board implements Serializable {
     private static final HashSet<Point> bottomLeftBasePoints;
     private static final HashSet<Point> topLeftBasePoints;
 
+    /**
+     * Przechowuje ID graczy (przeciwników), do których bazy musimy wejść, aby wygrać.
+     * Odpowiednio dla pierwszego gracza jest to pierwsza pozycja w tabeli, drugiego druga ...
+     */
     private int[] opponents;
 
-    private Point lastMoveDestination;
-    private boolean isLastMoveJump;
-
+    /**
+     * Domyślny konstruktor, inicjalizuje wartości domyślne.
+     */
     public Board() {
         numberOfPlayers = 0;
         opponents = null;
-        lastMoveDestination = null;
         piecesWithPosition = new HashMap<>();
     }
 
+    /**
+     * Inicjalizuje stałe informacje o planszy (Sety: wszystkich pozycji, poszczególnych rogów)
+     */
     static {
         validPoints = generateAllValidPoints();
         topBasePoints = generateTopBasePoints();
@@ -46,11 +68,24 @@ public class Board implements Serializable {
         topLeftBasePoints = generateTopLeftBasePoints();
     }
 
+    /**
+     * Zmienia pozycję pionka z oldPosition na newPosition.
+     *
+     * @param oldPosition stara pozycja
+     * @param newPosition nowa pozycja
+     */
     public void movePiece(Point oldPosition, Point newPosition) {
         piecesWithPosition.put(newPosition, piecesWithPosition.get(oldPosition));
         piecesWithPosition.remove(oldPosition);
     }
 
+    /**
+     * Sprawdza czy oldPoint znajduje się w rogu docelowym i czy newPoint znajduje się poza nim.
+     *
+     * @param oldPoint stara pozycja
+     * @param newPoint nowa pozycja
+     * @return
+     */
     public boolean isLeavingOpponentBase(Point oldPoint, Point newPoint) {
         int playerID = this.getPlayerIDAt(oldPoint);
         return this.getOpponentBaseCoordinates(playerID).contains(oldPoint) &&
@@ -58,30 +93,69 @@ public class Board implements Serializable {
 
     }
 
+    /**
+     * Sprawdza czy point jest na liście wszystkich pozycji planszy.
+     *
+     * @param point pozycja
+     * @return
+     */
     public boolean isValidPoint(Point point) {
         return validPoints.contains(point);
     }
 
+    /**
+     * Zwraca liczbę graczy.
+     *
+     * @return
+     */
     public int getNumberOfPlayers() {
         return numberOfPlayers;
     }
 
+    /**
+     * Zwraca pionek na pozycji point
+     *
+     * @param point pozycja
+     * @return
+     */
     public Piece getPieceAt(Point point) {
         return piecesWithPosition.get(point);
     }
 
+    /**
+     * Zwraca id gracza, którego pionek jest na pozycji point
+     *
+     * @param point pozycja
+     * @return
+     */
     public int getPlayerIDAt(Point point) {
         return getPieceAt(point).getPlayerNumber();
     }
 
+    /**
+     * zwraca wszystkie pozycje na planszy.
+     *
+     * @return
+     */
     public Set<Point> getValidPositions() {
         return validPoints;
     }
 
+    /**
+     * Zwraca wszystkie pozycje, na których są pionki.
+     *
+     * @return
+     */
     public Set<Point> getAllPiecesPositions() {
         return piecesWithPosition.keySet();
     }
 
+    /**
+     * Zwraca pozycje, na których stoją pionki gracza z playerID
+     *
+     * @param playerID id gracza
+     * @return
+     */
     public Set<Point> getPlayerPiecesCoordinates(int playerID) {
         HashSet<Point> playerPieces = new HashSet<>();
         piecesWithPosition.forEach((key, value) -> {
@@ -92,10 +166,22 @@ public class Board implements Serializable {
         return playerPieces;
     }
 
+    /**
+     * Zwraca id przeciwnika, do kttórego bazy musimy dojść.
+     *
+     * @param playerID id gracza
+     * @return
+     */
     public int getOpponentBaseNumber(int playerID) {
         return opponents[playerID - 1];
     }
 
+    /**
+     * Zwraca set pozycji z bazy docelowej (przeciwnika).
+     *
+     * @param playerID
+     * @return
+     */
     public Set<Point> getOpponentBaseCoordinates(int playerID) {
         int baseNumber = getOpponentBaseNumber(playerID);
         switch (baseNumber) {
@@ -121,6 +207,9 @@ public class Board implements Serializable {
         }
     }
 
+    /**
+     * Ustawia id przeciwników odpowiednio dla ilości graczy.
+     */
     private void setOpponents() {
         this.opponents = switch (numberOfPlayers) {
             case 2 -> new int[]{4, 1};
@@ -131,10 +220,18 @@ public class Board implements Serializable {
         };
     }
 
+    /**
+     * Usuwa wszystkie pionki graczy z planszy.
+     */
     private void clearBoard() {
         piecesWithPosition.clear();
     }
 
+    /**
+     * Tworzy Set wszystkich pól na planszy.
+     *
+     * @return
+     */
     private static HashSet<Point> generateAllValidPoints() {
         HashSet<Point> points = new HashSet<>();
         int mxMidPoint = 12;
@@ -154,6 +251,11 @@ public class Board implements Serializable {
         return points;
     }
 
+    /**
+     * Tworzy Set startowych pozycji z górnego rogu planszy.
+     *
+     * @return
+     */
     private static HashSet<Point> generateTopBasePoints() {
         HashSet<Point> points = new HashSet<>();
         int xMidPoint = 12;
@@ -167,6 +269,11 @@ public class Board implements Serializable {
         return points;
     }
 
+    /**
+     * Tworzy Set startowych pozycji z górnego prawego rogu planszy.
+     *
+     * @return
+     */
     private static HashSet<Point> generateTopRightBasePoints() {
         HashSet<Point> points = new HashSet<>();
         int xMidPoint = 21;
@@ -181,6 +288,11 @@ public class Board implements Serializable {
         return points;
     }
 
+    /**
+     * Tworzy Set startowych pozycji z dolnego prawego rogu planszy.
+     *
+     * @return
+     */
     private static HashSet<Point> generateBottomRightBasePoints() {
         HashSet<Point> points = new HashSet<>();
         int xMidPoint = 21;
@@ -194,6 +306,11 @@ public class Board implements Serializable {
         return points;
     }
 
+    /**
+     * Tworzy Set startowych pozycji z dolnego rogu planszy.
+     *
+     * @return
+     */
     private static HashSet<Point> generateBottomBasePoints() {
         HashSet<Point> points = new HashSet<>();
         int xMidPoint = 12;
@@ -207,6 +324,11 @@ public class Board implements Serializable {
         return points;
     }
 
+    /**
+     * Tworzy Set startowych pozycji z dolnego lewego rogu planszy.
+     *
+     * @return
+     */
     private static HashSet<Point> generateBottomLeftBasePoints() {
         HashSet<Point> points = new HashSet<>();
         int xMidPoint = 3;
@@ -220,6 +342,11 @@ public class Board implements Serializable {
         return points;
     }
 
+    /**
+     * Tworzy Set startowych pozycji z górnego lewego rogu planszy.
+     *
+     * @return
+     */
     private static HashSet<Point> generateTopLeftBasePoints() {
         HashSet<Point> points = new HashSet<>();
         int xMidPoint = 3;
@@ -233,6 +360,11 @@ public class Board implements Serializable {
         return points;
     }
 
+    /**
+     * Ustawia pionki gracza z playerID na pozycji pierwszej (górny róg)
+     *
+     * @param playerID
+     */
     private void setPiecesAtPositionOne(int playerID) {
         topBasePoints.forEach(point -> {
             Piece piece = new Piece(playerID);
@@ -240,6 +372,11 @@ public class Board implements Serializable {
         });
     }
 
+    /**
+     * Ustawia pionki gracza z playerID na pozycji drugiej (górny prawy róg)
+     *
+     * @param playerID
+     */
     private void setPiecesAtPositionTwo(int playerID) {
         topRightBasePoints.forEach(point -> {
             Piece piece = new Piece(playerID);
@@ -247,6 +384,11 @@ public class Board implements Serializable {
         });
     }
 
+    /**
+     * Ustawia pionki gracza z playerID na pozycji trzeciej (dolny prawy róg)
+     *
+     * @param playerID
+     */
     private void setPiecesAtPositionThree(int playerID) {
         bottomRightBasePoints.forEach(point -> {
             Piece piece = new Piece(playerID);
@@ -254,6 +396,11 @@ public class Board implements Serializable {
         });
     }
 
+    /**
+     * Ustawia pionki gracza z playerID na pozycji czwartej (dolny róg)
+     *
+     * @param playerID
+     */
     private void setPiecesAtPositionFour(int playerID) {
         bottomBasePoints.forEach(point -> {
             Piece piece = new Piece(playerID);
@@ -261,6 +408,11 @@ public class Board implements Serializable {
         });
     }
 
+    /**
+     * Ustawia pionki gracza z playerID na pozycji piątej (dolny lewy róg)
+     *
+     * @param playerID
+     */
     private void setPiecesAtPositionFive(int playerID) {
         bottomLeftBasePoints.forEach(point -> {
             Piece piece = new Piece(playerID);
@@ -268,6 +420,11 @@ public class Board implements Serializable {
         });
     }
 
+    /**
+     * Ustawia pionki gracza z playerID na pozycji szóstej (górny lewy róg).
+     *
+     * @param playerID
+     */
     private void setPiecesAtPositionSix(int playerID) {
         topLeftBasePoints.forEach(point -> {
             Piece piece = new Piece(playerID);
@@ -275,6 +432,10 @@ public class Board implements Serializable {
         });
     }
 
+    /**
+     * Dodaje gracza: Zwraca id dla tego gracza, zwiększa liczbę graczy,ustawia pionki na odpowiednich dla graczy pozycjach.
+     * @return
+     */
     public int addPlayer() {
         numberOfPlayers++;
         switch (numberOfPlayers) {
@@ -328,6 +489,11 @@ public class Board implements Serializable {
         return numberOfPlayers;
     }
 
+    /**
+     * Sprawdza czy gracz z playerID wygrał grę.
+     * @param playerID ID gracza
+     * @return
+     */
     public boolean checkForWin(int playerID) {
         return getPlayerPiecesCoordinates(playerID)
                 .equals(getOpponentBaseCoordinates(playerID));
