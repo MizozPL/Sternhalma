8f4b5766a80c6712b59c9b0c78229d63e24eb14e
MizozPl <dox44@wp.pl>
2022-01-11 01:18:03 +0100

    GameManager.java javadoc

--- src/main/java/com/sternhalma/server/games/GameManager.java
+++ src/main/java/com/sternhalma/server/games/GameManager.java
@@ -5,19 +5,41 @@ import com.sternhalma.server.connection.Player;
 
 import java.util.HashMap;
 
+/**
+ * Klasa zarządzająca wszystkimi istniejącymi grami na serwerze. Singleton.
+ */
 public class GameManager {
 
+    /**
+     * Instancja klasy.
+     */
     private static final GameManager instance = new GameManager();
+    /**
+     * Mapa kodów gier i ich instancji.
+     */
     private final HashMap<String, Game> games;
 
+    /**
+     * Prywatny konstruktor (singleton).
+     */
     private GameManager() {
         games = new HashMap<String, Game>();
     }
 
+    /**
+     * Zwraca instancję klasy.
+     * @return instancja klasy
+     */
     public static GameManager getInstance() {
         return instance;
     }
 
+    /**
+     * Tworzy nową grę o danym kodzie i typie.
+     * @param player handler twórcy gry
+     * @param gameID id gry, dowolny unikalny string
+     * @param gameType typ gry (z Games.java)
+     */
     public synchronized void createGame(Player player, String gameID, String gameType) {
         if (games.containsKey(gameID)) {
             player.sendMessage(NetworkMessages.GAME_WITH_ID_EXISTS);
@@ -32,6 +54,12 @@ public class GameManager {
         player.sendMessage(NetworkMessages.GAME_CREATED);
     }
 
+    /**
+     * Wykonuje akcję action przez gracza o handlerze player na grze gameID.
+     * @param player handler gracza wykonującego akcję
+     * @param gameID id gry, na której akcja jest wykonana
+     * @param action odpowiednio spreparowany String akcji
+     */
     public synchronized void performAction(Player player, String gameID, String action) {
         if (games.containsKey(gameID)) {
             games.get(gameID).performAction(player, action);
@@ -40,6 +68,11 @@ public class GameManager {
         player.sendMessage(NetworkMessages.GAME_WITH_ID_DOES_NOT_EXIST);
     }
 
+    /**
+     * Dołącza gracza o handlerze player do gry gameID.
+     * @param player handler gracza
+     * @param gameID id gry
+     */
     public synchronized void joinGame(Player player, String gameID) {
         if (games.containsKey(gameID)) {
             games.get(gameID).joinPlayer(player);
